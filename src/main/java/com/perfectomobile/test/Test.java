9e463fb35a1d0f81534f0cb67885bc6a4ef28bc7
Moshe Ben Shoham <mosheb@perfectomobile.com>
2014-02-19 14:24:04 +0000

    [maven-release-plugin]  copy for branch svn2git-test-2.19

---
+++ src/main/java/com/perfectomobile/test/Test.java
@@ -0,0 +1,13 @@
+package com.perfectomobile.test;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		String str = "hello";
+		String str2 = StringUtils.center(str, 20);
+		System.out.println(str2);
+	}
+
+}
