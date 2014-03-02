2e2186f1da33824a40769f110fcc34caf0011d65
Moshe Ben Shoham <mosheb@perfectomobile.com>
2014-03-02 14:59:43 +0200

    sss

--- src/main/java/com/perfectomobile/test/Test2.java
+++ src/main/java/com/perfectomobile/test/Test2.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 public class Test2 {
 	
 	public static void main(String[] args) {
-		String str = "hello4";
+		String str = "hello42";
 		String str2 = StringUtils.center(str, 20);
 		System.out.println(str2);
 	}
