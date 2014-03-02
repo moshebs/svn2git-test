70e9f35af7b553126f9466fda4d17e0738b5854d
BobPerfectomobile <bob@perfectomobile.com>
2014-03-02 16:19:19 +0200

    19 commit2

--- src/main/java/com/perfectomobile/test/Test.java
+++ src/main/java/com/perfectomobile/test/Test.java
@@ -5,7 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 public class Test {
 	
 	public static void main(String[] args) {
-		String str = "hello1115111";
+		String str = "hello11151112";
 		String str2 = StringUtils.center(str, 20);
 		System.out.println(str2);
 	}
